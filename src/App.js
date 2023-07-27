8a08fc9834679fbd8c908efb49a504396d130480
Chetan Patil <822chetanpatil@gmail.com>
2023-07-27 17:45:30 +0530

    new challenge

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import Navbar from "./Components/Navbar.js"
-import './App.css';
-import User from "./Pages/User.js"
-import {Routes,Route} from "react-router-dom"
-import Register from "./Pages/Register.js"
-import Save from "./Pages/Save.js";
-import Footer from "./Components/Footer.js";
-import Home from "./Pages/Home.js"
-import Page1api from "./Pages/Page1api.js";
-import Savedrecipe from "./Pages/Savedrecipe.js";
-import Savedrecipea from "./Pages/Savedrecipea.js";
-import Category from "./Pages/Category.js"
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/MainComponents/Home'
+import PageNotFound from './Components/MainComponents/PageNotFound'
+import SearchPage from './Components/RedirectedPages/SearchPage'
+import RecipesCategoriesPage1 from './Components/RedirectedPages/RecipesCategoriesPage1'
+import LoginPage from './Components/LoginRegister/LoginPage'
+import RegisterPage from './Components/LoginRegister/RegisterPage'
+import UserProfilePage from './Components/RedirectedPages/UserProfilePage'
+import UserSettingsPage from './Components/RedirectedPages/UserSettingsPage'
+import AllCategoriesPage from './Components/RedirectedPages/AllCategoriesPage'
+import SiteMapPage from './Components/RedirectedPages/SiteMapPage'
+import AboutUsPage from './Components/RedirectedPages/AboutUsPage'
+import ScrollToTop from './Components/MainComponents/ScrollToTop'
+import SavedPageRecipes from './Components/RedirectedPages/SavedPageRecipes'
+import SavedPageMyBoard from './Components/RedirectedPages/SavedPageMyBoard'
+import AddYourOwnRecipePage from './Components/RedirectedPages/AddYourOwnRecipePage'
+import RecipeCategoriesDescriptionPage from './Components/RedirectedPages/RecipeCategoriesDescriptionPage'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [recipe, setRecipe] = useState([]);
+  console.log(recipe)
+  const saveRecipe = (data) => {
+    setRecipe([...recipe, { ...data }])
+  }
+
   return (
-    <div className="App">
-   <Navbar/>
- 
-    <Routes>
-    <Route path='/recipes/:recipename' element={<Category />}></Route>
-      <Route path="/auth" element={<User/>}/>
-    <Route path="/auth/register" element={<Register/>}/>
-    <Route path="/save" element={<Save/>}/>
-    <Route path="/savedrecipe" element={<Savedrecipe/>}/>
-    <Route path="/savedrecipea" element={<Savedrecipea/>}/>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/api1" element={<Page1api/>}/></Routes>
-    <Footer/>
-    </div>
-  );
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/recipes/:recipename' element={<RecipesCategoriesPage1 />}></Route>
+        <Route path='/search' element={<SearchPage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/register' element={<RegisterPage />}></Route>
+        <Route path='/userprofile/:filtername' element={<UserProfilePage />}></Route>
+        <Route path='/addyourownrecipe' element={<AddYourOwnRecipePage />}></Route>
+        <Route path='/usersettings' element={<UserSettingsPage />}></Route>
+        <Route path='/allcategories' element={<AllCategoriesPage />}></Route>
+        <Route path='/sitemap' element={<SiteMapPage />}></Route>
+        <Route path='/aboutus' element={<AboutUsPage />}></Route>
+        <Route path='/saved/recipes' element={<SavedPageRecipes recipe={recipe} />}></Route>
+        <Route path='/saved/myboards' element={<SavedPageMyBoard />}></Route>
+        <Route path='/recipedescription/:recipename' element={<RecipeCategoriesDescriptionPage saveRecipe={saveRecipe} />}></Route>
+
+
+
+
+
+
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/Hooks/Usegetuserid.js
+++
@@ -1,5 +0,0 @@
-export const Usegetuserid=()=>{
-    return(
-        window.localStorage.getItem("userId")
-    )
-}
